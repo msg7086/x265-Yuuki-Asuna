914ef4506195683417d78b20be8b01df35d97321
Xinyue Lu <i@7086.in>
2015-03-18 01:43:57 -0700

    Output: set level of writing options in SEI

--- source/x265.h
+++ source/x265.h
@@ -1052,6 +1052,8 @@ typedef struct x265_param
      * Default is 0, which is recommended */
     int       crQpOffset;
 
+    int opts;
+
     struct
     {
         /* Explicit mode of rate-control, necessary for API users. It must
