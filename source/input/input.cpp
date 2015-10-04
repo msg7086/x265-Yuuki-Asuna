131830065ddc16d79542025564cfaaef47ee5b80
Xinyue Lu <i@7086.in>
2015-10-04 04:30:00 -0400

    Input: mp4 & mkv through lavf

--- source/input/input.cpp
+++ source/input/input.cpp
@@ -24,6 +24,9 @@
 #include "input.h"
 #include "yuv.h"
 #include "y4m.h"
+#ifdef ENABLE_LAVF
+  #include "lavf.h"
+#endif
 
 using namespace X265_NS;
 
@@ -33,6 +36,12 @@ InputFile* InputFile::open(InputFileInfo& info, bool bForceY4m)
 
     if (bForceY4m || (s && !strcmp(s, ".y4m")))
         return new Y4MInput(info);
-    else
-        return new YUVInput(info);
+#ifdef ENABLE_LAVF
+    if (s &&
+        ( !strcmp(s, ".mp4")
+        ||!strcmp(s, ".mkv")
+        ))
+        return new LavfInput(info);
+#endif
+    return new YUVInput(info);
 }
