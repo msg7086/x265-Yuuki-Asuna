549a7e071694c814340dde22727be4fcc8282ecd
Xinyue Lu <i@7086.in>
2016-07-28 00:55:15 -0400

    Param: tune LP

--- source/x265cli.h
+++ source/x265cli.h
@@ -400,6 +400,8 @@ static void showHelp(x265_param *param)
     H0("-p/--preset <string>             Trade off performance for compression efficiency. Default medium\n");
     H0("                                 ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, or placebo\n");
     H0("-t/--tune <string>               Tune the settings for a particular type of source or situation:\n");
+    H0("             (mid bitrate anime) littlepox/lp, (slower) littlepox++/lp++,\n");
+    H0("  (high bitrate anime BD / film) vcb-s/vcbs,   (slower) vcb-s++/vcbs++,\n");
     H0("                                 psnr, ssim, grain, zerolatency, fastdecode\n");
     H0("\nQuad-Tree size and depth:\n");
     H0("-s/--ctu <64|32|16>              Maximum CU size (WxH). Default %d\n", param->maxCUSize);
