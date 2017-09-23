5b771abd049ac4b3c827c372876fba7cba082d1d
Xinyue Lu <i@7086.in>
2017-09-23 00:25:21 -0400

    Output: mp4 through l-smash

--- source/x265cli.cpp
+++ source/x265cli.cpp
@@ -28,6 +28,10 @@
 #include "x265cli.h"
 #include "svt.h"
 
+#ifdef ENABLE_LSMASH
+#include <lsmash.h>
+#endif
+
 #define START_CODE 0x00000001
 #define START_CODE_BYTES 4
 
@@ -39,6 +43,9 @@ namespace X265_NS {
     {
         x265_log(param, X265_LOG_INFO, "HEVC encoder version %s\n", api->version_str);
         x265_log(param, X265_LOG_INFO, "build info %s\n", api->build_info_str);
+        #ifdef ENABLE_LSMASH
+            x265_log(param, X265_LOG_INFO, "(lsmash %d.%d.%d)\n", LSMASH_VERSION_MAJOR, LSMASH_VERSION_MINOR, LSMASH_VERSION_MICRO);
+        #endif
     }
 
     static void showHelp(x265_param *param)
@@ -57,7 +64,11 @@ namespace X265_NS {
         H0("   --fullhelp                    Show all options and exit\n");
         H0("-V/--version                     Show version info and exit\n");
         H0("\nOutput Options:\n");
-        H0("-o/--output <filename>           Bitstream output file name\n");
+        H0("-o/--output <filename>           Output file name. Default is raw bitstream"
+#ifdef ENABLE_LSMASH
+            ", MP4 if *.mp4"
+#endif
+            "\n");
         H0("-D/--output-depth 8|10|12        Output bit depth (also internal bit depth). Default %d\n", param->internalBitDepth);
         H0("   --log-level <string>          Logging level: none error warning info debug full. Default %s\n", X265_NS::logLevelNames[param->logLevel + 1]);
         H1("   --log-file <filename>         Save log to file\n" );
