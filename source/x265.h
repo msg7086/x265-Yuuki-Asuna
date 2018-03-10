8fc7933f836a7fb9f87da279716dee9570ee94eb
Xinyue Lu <i@7086.in>
2018-03-10 17:17:05 -0500

    Output: set level of writing options in SEI

--- source/x265.h
+++ source/x265.h
@@ -1341,6 +1341,8 @@ typedef struct x265_param
 	 * */
 	int       pictureStructure;	
 
+    int opts;
+
     struct
     {
         /* Explicit mode of rate-control, necessary for API users. It must
