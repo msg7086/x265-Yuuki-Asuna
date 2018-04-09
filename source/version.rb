eacbdc3eadee623e3bc6a8b003443117922cc429
Xinyue Lu <i@7086.in>
2018-04-09 01:50:56 -0400

    Cosmetic: update detailed version

--- source/version.rb
+++ source/version.rb
@@ -1,7 +1,8 @@
 light = ARGV.first == 'light'
 
+nickname = `git rev-parse --abbrev-ref HEAD`.strip
 vbase, vhead, vhcommit = `git describe --tags HEAD`.strip.split('-')
-branch = vbase['M'] ? 'master' : 'stable'
+branch = nickname == 'Yuuki' ? 'stable' : 'old-stable'
 _, vmaster, vmcommit = `git describe --tags origin/#{branch}`.strip.split('-')
 
 version = ''
