ce056fa39f38cd8f17608cca2fe5bce93649d4a5
Xinyue Lu <lu@kubisys.com>
2018-05-31 14:42:09 -0400

    Cosmetic: more detailed version

---
+++ source/version.rb
@@ -0,0 +1,17 @@
+light = ARGV.first == 'light'
+
+nickname = `git rev-parse --abbrev-ref HEAD`.strip
+vbase, vhead, vhcommit = `git describe --tags --first-parent HEAD`.strip.split('-')
+branch = nickname == 'Yuuki' ? 'stable' : 'old-stable'
+_, vmaster, vmcommit = `git describe --tags --first-parent origin/#{branch}`.strip.split('-')
+
+version = ''
+tag = vbase.delete('M')
+
+if vhcommit == vmcommit
+  version = "#{vbase}+#{vmaster}-#{vmcommit}"
+else
+  vdiff = vhead.to_i - vmaster.to_i
+  version = "#{vbase}+#{vmaster}-#{vmcommit}+#{vdiff}"
+end
+puts light ? tag : version
