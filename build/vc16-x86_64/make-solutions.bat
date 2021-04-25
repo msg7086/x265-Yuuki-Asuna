e6a4d66abd0003f9b7b31dcbc8e16c281abfc347
Automne von Einzbern <marwan@automne.me>
2021-04-25 20:41:55 +0200

    Removing arch specification

--- build/vc16-x86_64/make-solutions.bat
+++ build/vc16-x86_64/make-solutions.bat
@@ -3,4 +3,4 @@
 :: run this batch file to create a Visual Studio solution file for this project.
 :: See the cmake documentation for other generator targets
 ::
-cmake -G "Visual Studio 16 Win64" ..\..\source && cmake-gui ..\..\source
+cmake -G "Visual Studio 16 2019" ..\..\source && cmake-gui ..\..\source
